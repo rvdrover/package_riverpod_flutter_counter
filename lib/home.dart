a0d605172b7db8e4846dd6dcada4c072391f940c
rvdrover <53379496+rvdrover@users.noreply.github.com>
2021-11-15 21:08:11 +0530

    changes

--- lib/home.dart
+++ lib/home.dart
@@ -1,45 +1,56 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_riverpod_flutter_counter/providers/counter_provider.dart';
 
-class MyHomePage extends ConsumerWidget {
+ final counterProvider = StateNotifierProvider<CounterProvider, int>((ref) =>CounterProvider());
+
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
-    final counterProvider = StateProvider((ref) => 0);
+  Widget build(BuildContext context) {
+   
     print("Widget Rebuild");
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          "$counterProvider.state",
-          style: const TextStyle(
-            fontSize: 60,
-            fontWeight: FontWeight.bold,
+      body: Consumer(builder: (context, ref, child) {
+        final count = ref.watch(counterProvider);
+        return Center(
+          child: Text(
+            "$count",
+            style: const TextStyle(
+              fontSize: 60,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: () => {},
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          FloatingActionButton(
-            onPressed: () => {},
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          )
-        ],
+        );
+      }),
+      floatingActionButton: Consumer(
+        builder: (context, ref, child) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: () => ref.read(counterProvider.notifier).increment(),
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              FloatingActionButton(
+                onPressed: () => ref.read(counterProvider.notifier).decrement(),
+                tooltip: 'Decrement',
+                child: const Icon(Icons.remove),
+              )
+            ],
+          );
+        }
       ),
     );
   }
